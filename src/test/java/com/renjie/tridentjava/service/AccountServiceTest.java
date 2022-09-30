b4a011ec5a474c9e57857a18bb4037d5d70658db
allenwalker-rj <734403439@qq.com>
2022-09-30 17:32:18 +0800

    :white_check_mark: add account test

--- src/test/java/com/renjie/tridentjava/service/AccountServiceTest.java
+++ src/test/java/com/renjie/tridentjava/service/AccountServiceTest.java
@@ -1,8 +1,11 @@
 package com.renjie.tridentjava.service;
 
+import com.renjie.tridentjava.config.CommonConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.tron.trident.proto.Response;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 class AccountServiceTest {
 
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private CommonConfig config;
     @Test
     void getAccount() {
+        Response.Account account = accountService.getAccount(config.getAddress());
+        log.info("account:{}", account);
     }
 }
